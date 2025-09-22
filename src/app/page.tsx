eeadfb2cf382f85738785f9f471718f38b35d005
Willian Rodrigues <w.almeida.w@outlook.com>
2025-09-22 16:13:07 -0300

    refactor: organize Home component items into MenuGroups for better structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,41 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-dvh">
-      <div className="min-w-lg flex gap-2 flex-col">
-        <Item title="Guia consórcio" href="/consorcio" />
-        <Item
-          title="Card Carousel"
-          href="/card?mode=compact&headline=FINAL%204045&title=Cart%C3%A3o%20de%20Cr%C3%A9dito%20Visa&subtitle=vencimento%2010/25&value=R$%20100,00&tagName=em%20dia"
-        />
+      <div className="min-w-lg">
+        <MenuGroup title="Vetores">
+          <Item title="Guia consórcio" href="/consorcio" />
+        </MenuGroup>
+        <MenuGroup title="JSON">
+          <Item title="Ofertas renegociação" href="/cobranca/oferta" />
+        </MenuGroup>
+        <MenuGroup title="Outros">
+          <Item
+            title="Card Carousel Compact"
+            href="/card?mode=compact&headline=FINAL%204045&title=Cart%C3%A3o%20de%20Cr%C3%A9dito%20Visa&subtitle=vencimento%2010/25&value=R$%20100,00&tagName=em%20dia"
+          />
+          <Item
+            title="Card Carousel Full"
+            href="/card?mode=full&headline=FINAL%204045&title=Cart%C3%A3o%20de%20Cr%C3%A9dito%20Visa&subtitle=vencimento%2010/25&value=R$%20100,00&tagName=em%20dia"
+          />
+        </MenuGroup>
       </div>
+    </div>
+  );
+}
+
+function MenuGroup({
+  children,
+  title,
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) {
+  return (
+    <div className="w-full flex flex-col gap-2 p-4">
+      {title && <h3 className="text-lg font-semibold">{title}</h3>}
+      {children}
     </div>
   );
 }
