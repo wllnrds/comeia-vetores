81394b7fa97c1f73c76360f30a55ba44f7391b18
Willian Rodrigues <w.almeida.w@outlook.com>
2025-09-22 13:32:33 -0300

    refactor: update Home component layout and add link to consórcio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"></div>
+    <div className="flex items-center justify-center min-h-dvh">
+      <div className="min-w-lg">
+        <div className="center bg-indigo-600 hover:bg-indigo-700 pointer flex flex-col p-6 rounded-lg gap-4">
+          <Link target="_blank" href="/consorcio" className="text-white">
+            Guia consórcio
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
