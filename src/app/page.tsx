774092b2416a8c61885d91122e5dbf57571715c8
Willian Rodrigues <w.almeida.w@outlook.com>
2025-09-23 10:06:57 -0300

    feat: implement dynamic MenuGroup and Item components with structured data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,89 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+
+type IGroup = {
+  name: string;
+  type?: "grid" | "default";
+  items: {
+    title: string;
+    href: string;
+    cover?: string;
+  }[];
+};
+
+const data: IGroup[] = [
+  {
+    name: "Vetores",
+    items: [
+      {
+        title: "Guia consórcio",
+        href: "/consorcio",
+      },
+    ],
+  },
+  {
+    name: "JSON",
+    items: [
+      {
+        title: "Ofertas renegociação",
+        href: "/cobranca/oferta",
+      },
+    ],
+  },
+  {
+    name: "Cards",
+    type: "grid",
+    items: [
+      {
+        title: "Card Carousel Compact",
+        cover:
+          "/card?mode=compact&headline=FINAL%204045&title=Cart%C3%A3o%20de%20Cr%C3%A9dito%20Visa&subtitle=vencimento%2010/25&value=R$%20100,00&tagName=em%20dia",
+        href: "/card?mode=compact&headline=FINAL%204045&title=Cart%C3%A3o%20de%20Cr%C3%A9dito%20Visa&subtitle=vencimento%2010/25&value=R$%20100,00&tagName=em%20dia",
+      },
+      {
+        title: "Card Carousel Full",
+        cover:
+          "/card?mode=full&headline=FINAL%204045&title=Cart%C3%A3o%20de%20Cr%C3%A9dito%20Visa&subtitle=vencimento%2010/25&value=R$%20100,00&tagName=em%20dia",
+        href: "/card?mode=full&headline=FINAL%204045&title=Cart%C3%A3o%20de%20Cr%C3%A9dito%20Visa&subtitle=vencimento%2010/25&value=R$%20100,00&tagName=em%20dia",
+      },
+      {
+        title: "Progress Card %",
+        cover:
+          "/progress?title=Progresso&current=50&total=100&label=50%25%20contemplado&colorBackground=%23F0F0F0&colorProgress=linear-gradient%28201deg%2Crgba%28174%2C%20227%2C%20238%2C%201%29%200%25%2C%20rgba%28148%2C%20162%2C%20233%2C%201%29%20100%25%29&colorText=%234F46E5",
+        href: "/progress?title=Progresso&current=50&total=100&label=50%25%20contemplado&colorBackground=%23F0F0F0&colorProgress=linear-gradient%28201deg%2Crgba%28174%2C%20227%2C%20238%2C%201%29%200%25%2C%20rgba%28148%2C%20162%2C%20233%2C%201%29%20100%25%29&colorText=%234F46E5",
+      },
+      {
+        title: "Progress Card unidade",
+        cover:
+          "/progress?title=Progresso&current=10&total=12&label=1%20de%2012&colorBackground=%23F0F0F0&colorProgress=linear-gradient%28201deg%2Crgba%28174%2C%20227%2C%20238%2C%201%29%200%25%2C%20rgba%28148%2C%20162%2C%20233%2C%201%29%20100%25%29&colorText=%234F46E5",
+        href: "/progress?title=Progresso&current=10&total=12&label=1%20de%2012&colorBackground=%23F0F0F0&colorProgress=linear-gradient%28201deg%2Crgba%28174%2C%20227%2C%20238%2C%201%29%200%25%2C%20rgba%28148%2C%20162%2C%20233%2C%201%29%20100%25%29&colorText=%234F46E5",
+      },
+    ],
+  },
+];
 
 export default function Home() {
   return (
     <div className="flex pt-4 justify-center min-h-dvh">
       <div className="min-w-lg">
-        <MenuGroup title="Vetores">
-          <Item title="Guia consórcio" href="/consorcio" />
-        </MenuGroup>
-        <MenuGroup title="JSON">
-          <Item title="Ofertas renegociação" href="/cobranca/oferta" />
-        </MenuGroup>
-        <MenuGroup title="Outros" className="flex-row gap-2 flex-wrap">
-          <Item
-            title="Card Carousel Compact"
-            cover="/card?mode=compact&headline=FINAL%204045&title=Cart%C3%A3o%20de%20Cr%C3%A9dito%20Visa&subtitle=vencimento%2010/25&value=R$%20100,00&tagName=em%20dia"
-            href="/card?mode=compact&headline=FINAL%204045&title=Cart%C3%A3o%20de%20Cr%C3%A9dito%20Visa&subtitle=vencimento%2010/25&value=R$%20100,00&tagName=em%20dia"
-          />
-          <Item
-            title="Card Carousel Full"
-            cover="/card?mode=full&headline=FINAL%204045&title=Cart%C3%A3o%20de%20Cr%C3%A9dito%20Visa&subtitle=vencimento%2010/25&value=R$%20100,00&tagName=em%20dia"
-            href="/card?mode=full&headline=FINAL%204045&title=Cart%C3%A3o%20de%20Cr%C3%A9dito%20Visa&subtitle=vencimento%2010/25&value=R$%20100,00&tagName=em%20dia"
-          />
-        </MenuGroup>
+        {data.map((group) => (
+          <MenuGroup
+            key={JSON.stringify(group)}
+            title={group.name}
+            type={group.type || "default"}
+          >
+            {group.items.map((item) => (
+              <Item
+                key={JSON.stringify(item)}
+                title={item.title}
+                href={item.href}
+                cover={item?.cover}
+              />
+            ))}
+          </MenuGroup>
+        ))}
       </div>
     </div>
   );
@@ -32,15 +93,24 @@ function MenuGroup({
   children,
   title,
   className,
+  type,
 }: {
   children: React.ReactNode;
-  title?: string;
+  title: string;
   className?: string;
+  type?: "grid" | "default";
 }) {
+  const classByType = {
+    grid: "grid grid-cols-1 md:grid-cols-2",
+    default: "flex flex-col",
+  };
+
   return (
-    <div className={`w-full flex flex-col gap-2 p-4`}>
+    <div className={`w-full p-4 flex flex-col gap-2`}>
       {title && <h3 className="text-lg font-semibold">{title}</h3>}
-      <div className={`w-full flex gap-2 ${className}`}>{children}</div>
+      <div className={`w-full flex gap-2 ${classByType[type || "default"]}`}>
+        {children}
+      </div>
     </div>
   );
 }
@@ -49,18 +119,23 @@ function Item({
   title,
   href,
   cover,
+  className,
 }: {
   title: string;
   href: string;
   cover?: string;
+  className?: string;
 }) {
   return (
-    <div className="center bg-indigo-600 flex flex-col rounded-xl gap-2 flex-1 p-1">
+    <div
+      className={`center bg-indigo-600 flex flex-col rounded-xl gap-2 w-full p-1 ${className}`}
+    >
       {cover && (
-        <div className="aspect-square w-full flex-none rounded-lg overflow-hidden relative">
-          <Image src={cover} alt={title} fill />
+        <div className="w-full flex-none rounded-lg overflow-hidden relative">
+          <Image src={cover} alt={title} width={256} height={256} />
         </div>
       )}
+
       <Link
         target="_blank"
         href={href}
